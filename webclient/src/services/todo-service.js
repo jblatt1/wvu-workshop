2cd26f4931500b3e8b3837f1566bda4d878f586b
Billy Hardy <billy92@gmail.com>
2017-09-11 01:06:30 -0400

    add services

---
+++ webclient/src/services/todo-service.js
@@ -0,0 +1,46 @@
+export default class TodoService {
+    constructor(host) {
+        this.host = host + '/todo/';
+    }
+
+    getAll() {
+        return fetch(this.host)
+            .then(response => response.json());
+    }
+
+    get(id) {
+        return fetch(this.host + id)
+            .then(response => response.json());
+    }
+
+    create(todo) {
+        return fetch(this.host, {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(todo)
+        })
+            .then(response => response.json());
+    }
+
+    update(todo) {
+        return Promise.resolve(todo);
+        return fetch(this.host + todo.id, {
+            method: 'PUT',
+            mode: 'cors',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(todo)
+        })
+            .then(response => response.json());
+    }
+
+    delete(id) {
+        return Promise.resolve();
+        return fetch(this.host + id, {
+            method: 'DELETE',
+        });
+    }
+}
