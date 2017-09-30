ffe118eea892d7ca59c1088ec095b2bd92425626
Jon Smithers <mail@jonsmithers.link>
2017-09-30 14:18:43 -0400

    Move html() definition into todo-app.js
    I might have have students write index.html from scratch and don't
    really want to explain why it's there.

--- webclient/src/elements/todo-app.js
+++ webclient/src/elements/todo-app.js
@@ -2,6 +2,7 @@ import {Element} from "../node_modules/@polymer/polymer/polymer-element.js"
 import todoService from "../services/todo-service.js"
 import "./todo-item.js"
 import "./todo-input.js"
+top.html = a=>a[0];
 
 export class TodoApp extends Element {
     static get is() {
